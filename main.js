283c88261f0c951782d51a8ac457a7050c7c708a
Willian Piantkoski <wjpiantkoski@gmail.com>
2021-01-04 20:36:34 -0300

    ServiceWorkerRegistration

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // progressive enhancement (sw supported)
 if (navigator.serviceWorker) {
   // register the sw
-  navigator.serviceWorker.register('/sw.js', { scope: '/posts/' })
+  navigator.serviceWorker.register('/sw.js')
     .then(function (registration) {
       console.log('sw registered')
     })
