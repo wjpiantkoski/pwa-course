420ea14de2325f7188b1fc7c758984b52118f47c
Willian Piantkoski <wjpiantkoski@gmail.com>
2021-01-04 21:45:48 -0300

    Events: push

--- sw.js
+++ sw.js
@@ -1,15 +1,3 @@
-self.addEventListener('message', e => {
-  // respond to all clients
-  self.clients.matchAll()
-    .then(clients => {
-      clients.forEach((client, i) => {
-        // only respond to sending client
-        if (e.source.id === client.id) {
-          client.postMessage('Private Hellow from SW ' + i)
-        }
-      })
-    })
-    .catch(err => {
-      console.error(err)
-    })
+self.addEventListener('push', () => {
+  console.log('push received')
 })
