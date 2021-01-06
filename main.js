ed5274fb534dbd372795c8286ba76476e466ad63
Willian Piantkoski <wjpiantkoski@gmail.com>
2021-01-05 21:33:56 -0300

    Notifications

--- main.js
+++ main.js
@@ -1,17 +1,36 @@
 // progressive enhancement (sw supported)
-if (navigator.serviceWorker) {
-  // register the sw
-  navigator.serviceWorker.register('/sw.js')
-    .then(function (registration) {
-      if (registration.active) {
-        registration.active.postMessage('respond to this')
+// if (navigator.serviceWorker) {
+//   // register the sw
+//   navigator.serviceWorker.register('/sw.js')
+//     .then(function (registration) {})
+//     .catch(function (err) {
+//       console.error(err)
+//     })
+// }
+
+// Notification support
+if (window.Notification) {
+  function showNotification() {
+    let options = {
+      body: 'Some notification information',
+      icon: '/icon.png'
+    }
+
+    let n = new Notification('My new notification', options)
+
+    n.onclick = () => {
+      console.log('notification clicked')
+    }
+  }
+
+  // manage permission
+  if (Notification.permission === 'granted') {
+    showNotification()
+  } else if (Notification.permission !== 'denied') {
+    Notification.requestPermission(permission => {
+      if (permission === 'granted') {
+        showNotification()
       }
     })
-    .catch(function (err) {
-      console.error(err)
-    })
-
-    navigator.serviceWorker.addEventListener('message', e => {
-      console.log(e.data)
-    })
+  }
 }
