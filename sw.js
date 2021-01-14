8c3753b814182d24dd0a0aec4c2845b7681b1240
Willian Piantkoski <wjpiantkoski@gmail.com>
2021-01-13 23:04:08 -0300

    caching strategies

--- sw.js
+++ sw.js
@@ -1,51 +1,98 @@
-const pwaCache = 'pwa-cache-2'
+const pwaCache = 'pwa-cache-1'
+const staticCache = ['/', '/index.html', '/style.css', '/main.js', '/thumb.png']
+
+// cache strategies
+self.addEventListener('fetch', e => {
+  /**
+   * 1. Cache only
+   * Good for statics assets (App shell)
+   */
+  //e.respondWith(caches.match(e.request))
+
+  /**
+   * 2. Cache with Network Fallback
+   * Same as 1 but can handle cache delete
+   */
+  // e.respondWith(caches.match(e.request).then(res => {
+  //   if (res) {
+  //     return res
+  //   } else {
+  //     return fetch(e.request).then(newRes => {
+  //       caches.open(pwaCache).then(cache => cache.put(e.request, newRes))
+  //       return newRes.clone()
+  //     })
+  //   }
+  // }))
+
+  /**
+   * 3. Network with cache fallback
+   * If its not possible to get content from network, go to the cache
+   */
+  // e.respondWith(fetch(e.request)
+  //   .then(res => {
+  //     // cache latest version
+  //     caches.open(pwaCache).then(cache => cache.put(e.request, res))
+  //     return res.clone()
+  //   })
+  //   .catch(err => caches.match(e.request)) // fallback to cache
+  // ) 
+
+  /**
+   * CAche with network update
+   * Go to the cache to serve user immediatly, then update content in the background
+   */
+  // e.respondWith(caches.open(pwaCache).then(cache => {
+  //   return cache.match(e.request).then(res => {
+  //     // update
+  //     let updatedResponse = fetch(e.request).then(newRes => {
+  //       // cache new response
+  //       cache.put(e.request, newRes.clone())
+  //       return newRes
+  //     })
+
+  //     return res || updatedResponse
+  //   })
+  // }))
+
+  /**
+   * 5. Cache and network race
+   */
+  // let firstResponse = new Promise((resolve, reject) => {
+  //   // track rejections
+  //   let firstRejectReceived = false
+  //   let rejectOnce = () => {
+  //     if (firstRejectReceived) {
+  //       reject('no responde received')
+  //     } else {
+  //       firstRejectReceived = true
+  //     }
+  //   }
+
+  //   // try network
+  //   fetch(e.request).then(res => {
+  //     res.ok ? resolve(res) : rejectOnce()
+  //   }).catch(rejectOnce)
+
+  //   // try cache
+  //   caches.match(e.request).then(res => {
+  //     res ? resolve(res) : rejectOnce()
+  //   }).catch(rejectOnce)
+  // })
+
+  // e.respondWith(firstResponse)
+})
 
 self.addEventListener('install', e => {
-  let cacheReady = caches.open(pwaCache).then(cache => {
-    console.log('new cache ready')
-    return cache.addAll([
-      '/',
-      'style.css',
-      'thumb.png',
-      'main.js'
-    ])
-  })
-
-  e.waitUntil(cacheReady)
+  e.waitUntil(caches.open(pwaCache).then(cache => cache.addAll(staticCache)))
 })
 
 self.addEventListener('activate', e => {
-  let cacheCleaned = caches.keys().then(keys => {
+  e.waitUntil(caches.keys().then(keys => {
     keys.forEach(key => {
       if (key !== pwaCache) {
         return caches.delete(key)
       }
     })
-  })
-
-  e.waitUntil(cacheCleaned)
+  }))
 })
 
-self.addEventListener('fetch', e => {
-  // skip for remote fetch
-  if (!e.request.url.match(location.origin)) return
-
-  // serve local fetch from cache
-  let newResponse = caches.open(pwaCache).then(cache => {
-    return cache.match(e.request).then(res => {
-      // check request was found in cache
-      if (res) {
-        console.log(`Serving ${res.url} from cache`)
-        return res
-      }
-
-      // fetch on behalf of client and cache
-      return fetch(e.request).then(fetchRes => {
-        cache.put(e.request, fetchRes.clone())
-        return fetchRes
-      })
-    })
-  })
-
-  e.respondWith(newResponse)
-})
